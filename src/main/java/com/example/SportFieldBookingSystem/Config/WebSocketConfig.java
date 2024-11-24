09b62321b844f88ad4c0ea05ab06544f450684d8
kietsocola <vankiet27012004@gmail.com>
2024-11-24 17:05:33 +0700

    hold timeslot realtime

--- src/main/java/com/example/SportFieldBookingSystem/Config/WebSocketConfig.java
+++ src/main/java/com/example/SportFieldBookingSystem/Config/WebSocketConfig.java
@@ -17,7 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173/").withSockJS();
     }
 }
 
--- src/main/java/com/example/SportFieldBookingSystem/Controller/TimeSlotController.java
+++ src/main/java/com/example/SportFieldBookingSystem/Controller/TimeSlotController.java
@@ -5,19 +5,28 @@ import com.example.SportFieldBookingSystem.DTO.TimeSlotDTO.TimeSlotCreateDTO;
 import com.example.SportFieldBookingSystem.DTO.TimeSlotDTO.TimeSlotResponseDTO;
 import com.example.SportFieldBookingSystem.DTO.TimeSlotDTO.TimeSlotUpdateDTO;
 import com.example.SportFieldBookingSystem.Entity.TimeSlot;
+import com.example.SportFieldBookingSystem.Enum.TimeSlotEnum;
 import com.example.SportFieldBookingSystem.Payload.ResponseData;
 import com.example.SportFieldBookingSystem.Service.TimeSlotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("api/timeSlot")
 public class TimeSlotController {
     private final TimeSlotService timeSlotService;
-
+    @Autowired
+    private TaskScheduler taskScheduler;
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
     @Autowired
     public TimeSlotController(TimeSlotService timeSlotService) {
         this.timeSlotService = timeSlotService;
@@ -114,6 +123,29 @@ public class TimeSlotController {
             return ResponseEntity.ok(responseData);
         } catch (Exception e) {
             responseData.setMessage("TimeSlot deletion failed");
+            responseData.setData(e);
+            return ResponseEntity.ok(responseData);
+        }
+    }
+
+    @PutMapping("/hold/{id}")
+    public ResponseEntity<?> holdTimeSlot(@PathVariable int id) {
+        ResponseData responseData = new ResponseData();
+        try {
+            TimeSlotResponseDTO timeSlot = timeSlotService.setStatus(id, TimeSlotEnum.PENDING);
+            if (timeSlot != null) {
+                responseData.setMessage("TimeSlot held successfully");
+                responseData.setData(timeSlot);
+                messagingTemplate.convertAndSend("/topic/holdTimeslot", id);
+                ScheduledFuture<?> scheduledTask = taskScheduler.schedule(() -> timeSlotService.setStatus(id, TimeSlotEnum.AVAILABLE),
+                        new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(1)));
+                return ResponseEntity.ok(responseData);
+            }
+            responseData.setMessage("TimeSlot held failed");
+            responseData.setData(null);
+            return ResponseEntity.ok(responseData);
+        } catch (Exception e) {
+            responseData.setMessage("TimeSlot held failed");
             responseData.setData(e);
             return ResponseEntity.ok(responseData);
         }
